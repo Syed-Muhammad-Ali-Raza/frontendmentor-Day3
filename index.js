a1c7e9f75941ba5e70837684bbb9ab4d24357cf7
Syed Muhammad Ali Raza <46846502+Syed-Muhammad-Ali-Raza@users.noreply.github.com>
2024-01-11 10:16:10 -0800

    Add files via upload
    newsletter UI in Html

---
+++ index.js
@@ -0,0 +1,12 @@
+// var btnClick = document.getElementById("#btnNews");
+
+
+// function clickButton(){
+ 
+//  if (  addEventListener('click' , btnClick))
+// {
+
+//  btnClick.style.color = "yellow";
+
+// }
+// }
